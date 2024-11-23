9182f350375778f6fada83235c399295b11dd2c6
mijail pulgar <mpulgar@abatechnology.com>
2024-11-23 13:05:53 -0500

    feat: trasformar a microservicio

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -25,10 +25,17 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async findAll(paginationDto: PaginationDto) {
     const { page, limit } = paginationDto;
-    const totalPages = await this.product.count();
+    const totalPages = await this.product.count({
+      where: {
+        available: true,
+      },
+    });
     const lastPage = Math.ceil(totalPages / limit);
     return {
       data: await this.product.findMany({
+        where: {
+          available: true,
+        },
         take: limit,
         skip: (page - 1) * limit,
       }),
@@ -40,6 +47,7 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     const product = await this.product.findFirst({
       where: {
         id,
+        available: true,
       },
     });
     if (!product) {
@@ -60,6 +68,14 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async remove(id: number) {
     await this.findOne(id);
-    return this.product.delete({ where: { id } });
+    // return this.product.delete({ where: { id } });
+    const product = await this.product.update({
+      where: { id },
+      data: {
+        available: false,
+      },
+    });
+
+    return product;
   }
 }
